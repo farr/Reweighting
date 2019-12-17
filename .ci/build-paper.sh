6e1edacdba3fa52a4fffe67d46f82175580196cf
Will M. Farr <wfarr-vscholar@flatironinstitute.org>
2019-12-16 21:44:33 -0500

    Don't check for changes---just build the damn paper (since we're already provisioning a VM at Travis).

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -x
 
-if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'note/'
-then
-  # Build the paper using LaTeX
-  cd note
-  latexmk -pdf reweighting
+# Build the paper using LaTeX
+cd note
+latexmk -pdf reweighting
 
-  # Force push the paper to GitHub
-  cd $TRAVIS_BUILD_DIR
-  git checkout --orphan $TRAVIS_BRANCH-pdf
-  git rm -rf .
-  git add -f note/reweighting.pdf
-  git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-  git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
-fi
+# Force push the paper to GitHub
+cd $TRAVIS_BUILD_DIR
+git checkout --orphan $TRAVIS_BRANCH-pdf
+git rm -rf .
+git add -f note/reweighting.pdf
+git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
+git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
